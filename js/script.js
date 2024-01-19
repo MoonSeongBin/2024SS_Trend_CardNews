8b794a97e429f9ab8b208dd4a65a048855203f44
MoonSeongBin <155716053+MoonSeongBin@users.noreply.github.com>
2024-01-19 15:39:09 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".main_banner", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
